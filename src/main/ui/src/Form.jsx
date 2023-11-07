fdef626f022a6a929b1e608f3f15204817c07a7e
Egorrrka1613 <reutov622@gmail.com>
2023-11-07 13:29:45 +0400

    style: add adaptive style to form

--- src/main/ui/src/Form.jsx
+++ src/main/ui/src/Form.jsx
@@ -1,21 +1,28 @@
-import React, {useState, useRef, useEffect, useCallback} from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
+import './Form.scss';
 
 const Form = () => {
-
     return (
-
-        <form method="POST" encType="multipart/form-data"
-              action="/api/creat">
-            File to upload: <input type="file" name="file"/>
-            <br/> pointCoordinates:
-            <input type="text" name="pointCoordinates"/>
-                <br/> <br/>
-            <br/> description:
-            <input type="text" name="description"/>
-            <br/> <br/>
-            <input type="submit" value="Upload"/> Press here to upload the file!
-        </form>
-);
+        <div className="form-container">
+            <form method="POST" encType="multipart/form-data" action="/api/create" className="upload-form">
+                <div className="form-group">
+                    <label htmlFor="file">Файл для загрузки:</label>
+                    <input type="file" id="file" name="file" className="form-control" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="pointCoordinates">Координаты точки:</label>
+                    <input type="text" id="pointCoordinates" name="pointCoordinates" className="form-control" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="description">Описание нарушения:</label>
+                    <input type="text" id="description" name="description" className="form-control" />
+                </div>
+                <div className="form-group">
+                    <button type="submit" className="btn-upload">Загрузить</button>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default Form;
