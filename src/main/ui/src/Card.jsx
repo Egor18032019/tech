05c8d82be0472867ed8f1619a73bb5199281129d
OsmanAlb <126768805+OsmanAlb@users.noreply.github.com>
2023-11-18 18:02:21 +0300

    Update Card.jsx

--- src/main/ui/src/Card.jsx
+++ src/main/ui/src/Card.jsx
@@ -1,11 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import './Card.scss';
 import {useContextMap} from "./PointReducer";
-import {url} from "./Const";
+// import {url} from "./Const";
 
 const Card = () => {
 
-    const [image, setImage] = useState(null);
+   const [image, setImage] = useState(null);
     const {point} = useContextMap();
     useEffect(() => {
         if (point != null) {
@@ -28,14 +28,14 @@ const Card = () => {
         setImage(data.url)
     };
 
-
+ 
     return (
         <div className="card-container">
 
             {point && (
                 <div className={`card card-${point.id}`}>
                     <p> {point.description}</p>
-                    <img src={`${image}`}/>
+                    <img src={`${image}`} width="300px" />
                 </div>
             )}
         </div>
