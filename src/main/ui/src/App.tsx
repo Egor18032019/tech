4c5dac660834349a80a909d19332df2dd4459d62
OsmanAlb <126768805+OsmanAlb@users.noreply.github.com>
2023-11-20 16:32:44 +0300

    Update App.tsx

--- src/main/ui/src/App.tsx
+++ src/main/ui/src/App.tsx
@@ -4,16 +4,21 @@ import Form from './Form';
 import './App.scss';
 import {TownProvider} from "./PointReducer";
 import Card from "./Card";
+import Nav from "./components/Nav/Nav";
 
 function App() {
     return (
-        <div>
-            <TownProvider>
-                <BranchesMap/>
-                <Card/>
-            </TownProvider>
-            <Form/>
-        </div>
+       <>
+        <Nav/>
+            <div>
+                
+                <TownProvider>
+                    <BranchesMap/>
+                    <Card/>
+                    <Form/>
+                </TownProvider>
+            </div>
+       </>      
     );
 }
 
